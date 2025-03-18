cc3e093162f74c9f0d9e4f3db38d668809172d71
Paul Rainsford <paulrainsford69@gmail.com>
2025-03-18 22:19:19 +0000

    Practice Pythjon

---
+++ name= input('What is your name?: ').py
@@ -0,0 +1,4 @@
+name= input('What is your name?: ')
+distance1= input('How far did you travel?: ')
+distance2= float(distance1) * 0.621371
+print('Hello ' + name.capitalize() + '. ' + 'You travelled: ' + str(distance1) + 'kms or ' + str(distance2) + 'mls')
